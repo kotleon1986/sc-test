5a55801e8ed5c1da7a24dbd2b62ec04b6602b7e7
Leonid Kotanidi <kotleon1986@gmail.com>
2022-03-18 21:41:22 +0400

    setup routing for frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsModule } from './pages/details/details.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'details',
+    loadChildren: () => DetailsModule    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
