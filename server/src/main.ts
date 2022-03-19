10f10b73a1533c09b2c2e7e40c9bd1291ac58225
Leonid Kotanidi <kotleon1986@gmail.com>
2022-03-19 14:14:50 +0400

    setup serverless configs for Lambda

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,30 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+import serverlessExpress from '@vendia/serverless-express';
+import { useContainer } from 'class-validator';
 
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
